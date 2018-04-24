375855fabb773740ba89f03d0b2882ff4dc0a24b
JMoloughney <github email address>
2018-04-24 11:53:19 -0400

    Done.

--- index.js
+++ index.js
@@ -4,20 +4,39 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+        for (let i = 0; i < newCollection.length; i++)
+        iteratee(newCollection[i])
+        return collection
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
 
+        const newArr = []
+
+        for (let i = 0; i < collection.length; i++)
+        newArr.push(iteratee(collection[i]))
+    return newArr
     },
 
-    reduce: function() {
-
+    reduce: function(collection, iteratee) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+      for (let i = 0; i < collection.length; i++)
+        acc = iteratee(acc, collection[idx], collection)
+      return acc
     },
-    
-    functions: function() {
 
+    functions: function(collection, iteratee) {
+      if (!(collection instanceof Array))
+        collection = Object.values(collection)
+      for (let idx = 0; i < collection.length; i++)
+        if (predicate(collection[idx])) return true
+      return false
     },
 
 
